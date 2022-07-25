6d5e618d689626b8f79e9223bde3a7a87aecf2c8
yeldrs <yass.elidrissi97@gmail.com>
2022-07-25 20:41:13 +0200

    fsefsf

--- html/neo4J.js
+++ html/neo4J.js
@@ -1,12 +1,44 @@
+let saveFile = () => {
+    	
+  // Get the data from each element on the form.
+const name = document.getElementById('01');
+
+
+  
+  // This variable stores all the data.
+  let data = 
+  '\r Name: ' + name.value + ' \r\n ' ;
+  
+  // Convert the text to BLOB.
+  const textToBLOB = new Blob([data], { type: 'text/plain' });
+  const sFileName = 'formData.txt';	   // The file to save the data.
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+      newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+      newLink.href = window.URL.createObjectURL(textToBLOB);
+      newLink.style.display = "none";
+      document.body.appendChild(newLink);
+  }
+
+  newLink.click(); 
+}
+/*
 function saveStaticDataToFile() {
 
-  var input = document.getElementById('userInput').value;
-  
-  alert(input)
+  var input = document.getElementById('01').value;
+
+alert(input)
   } 
 
-function addRecipe(){
 
+
+function addRecipe(){
+  (async() => {
     const neo4j = require('neo4j-driver')
     
     const uri = 'neo4j+s://75cf5522.databases.neo4j.io';
@@ -21,8 +53,6 @@ function addRecipe(){
     console.log(person1Name)
     console.log(person2Name)
 
-    alert(person1Name)
-    
     try {
       // To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
       // The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
@@ -60,5 +90,5 @@ function addRecipe(){
    
     // Don't forget to close the driver connection when you're finished with it
     await driver.close()
-   }
-  ;
+   })()
+  };*/
