2654299a08087b76aa0c96e939631fc0e294657c
Lazel102 <49607270+Lazel102@users.noreply.github.com>
2023-04-29 14:24:02 +0200

    Make registration and user pesistence functional.

--- Prototyp/src/main/java/com/example/prototyp/security/SecurityConfig.java
+++ Prototyp/src/main/java/com/example/prototyp/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.prototyp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,10 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 
   private UserService userService;
 
@@ -31,6 +30,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin()
         .and()
         .httpBasic();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService())
+        .passwordEncoder(passwordEncoder());
   }
 
   @Bean
--- Prototyp/src/main/resources/db/migration/V1__init_tables.sql
+++ Prototyp/src/main/resources/db/migration/V1__init_tables.sql
@@ -18,15 +18,7 @@ CREATE TABLE user_roles (
 );
 
 -- Insert default roles
-INSERT INTO roles (name) VALUES ('ROLE_USER');
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('USER');
+INSERT INTO roles (name) VALUES ('ADMIN');
 
--- Insert sample users (replace 'your_password' with desired password, properly hashed)
-INSERT INTO users (username, password) VALUES ('Yannilloni', '$2a$12$nTo4XE4e1bgyCRsOQZtOPulfojFqIuIGcVpjR4Q4rGdji/2FMFH/a
-');
-INSERT INTO users (username, password) VALUES ('Yasagne', '$2a$12$swg/hgzzSN3RntFlHMy0JOvHfGg7CR.Mj0B5TSdwONz9CUWFwMmzq
-');
 
--- Assign roles to sample users (assuming the roles have IDs 1 for ROLE_USER and 2 for ROLE_ADMIN)
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 2); -- user has ROLE_USER
-INSERT INTO user_roles (user_id, role_id) VALUES (2, 2); -- admin has ROLE_ADMIN
