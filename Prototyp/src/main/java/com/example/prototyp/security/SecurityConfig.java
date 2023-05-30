bd181f4a6fca5345411a1547f393eecb75a996af
Lazel102 <49607270+Lazel102@users.noreply.github.com>
2023-05-30 16:47:37 +0200

    repaired css

--- Prototyp/src/main/java/com/example/prototyp/security/SecurityConfig.java
+++ Prototyp/src/main/java/com/example/prototyp/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/", "/register").permitAll()
+        .antMatchers("/", "/register", "/css/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin()
